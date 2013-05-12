be24d364e908401af0b172843f162763ecd4c746
Jan Papoušek <jan.papousek@gmail.com>
2013-05-12 19:25:16 +0200

    further work on chapter about evaluation

--- scripts/image-parasim-scalability.r
+++ scripts/image-parasim-scalability.r
@@ -1,4 +1,4 @@
-directory <- "../data/parasim-2013-05-06/"
+directory <- "../data/parasim-2013-05-12/"
 models <- c("lotkav-common", "lotkav-iterations", "lotkav-long-property", "lorenz84-common", "lorenz84-iterations", "lorenz84-long-property", "meyer91-common")
 
 mar <- c(4, 4, 1, 1)
@@ -6,54 +6,75 @@ width <- 5
 height <- 4
 paper <- "special"
 
-par(mar = mar);
-
 for (model in models) {
 
-	summary <- read.csv(paste0(directory, model, "-summary.csv"));
-	summary <- summary[order(summary$environment.size), ];
-	iterations <- read.csv(paste0(directory, model, "-iterations.csv"));
-	iterations <- iterations[iterations$environment.size == 16, ]
+	data.summary <- read.csv(paste0(directory, model, "-summary.csv"));
+	data.summary <- data.summary[order(data.summary$environment.size), ];
+	data.iterations <- read.csv(paste0(directory, model, "-iterations.csv"));
 
-	# times
-	pdf(paste0("../images/generated/", model, "-time.pdf"), width = width, height = height)
-	plot(summary$environment.size, summary$time, pch=18, ylab="Time (s)", log = "x", xlab="Machines", ylim=c(0, max(summary$time)), xlim=c(1, 16), xaxt = "n");
-	lines(summary$environment.size, summary$time);
-	size_min <- min(summary$environment.size);
-	size_max <- max(summary$environment.size);
-	time_max <- max(summary$time);
-	f <- function(x) {
-		y = (time_max / x) * size_min
-	}
-	curve(f, from = size_min, to = size_max, col="red", add=TRUE, lty=2)
-	axis(side = 1, at = c(1, 2, 4, 8, 16))
-	dev.off();
-
-	# cache
-	pdf(paste0("../images/generated/", model, "-cache.pdf"), width = width, height = height)
-	plot(summary$environment.size, summary$cache.secondary.miss, pch=18, ylab="Cache miss", xlab="Machines", log = "x", xlim=c(1, 16), xaxt = "n");
-	lines(summary$environment.size, summary$cache.secondary.miss);
-	axis(side = 1, at = c(1, 2, 4, 8, 16))
-	dev.off();
-
-	# iterations
-	pdf(paste0("../images/generated/", model, "-iterations-primary.pdf"), width = width, height = height)
-	plot(iterations$iteration, iterations$primary, pch=18, ylab="Trajectories", xlab="Iteration");
-	lines(iterations$iteration, iterations$primary);
-	dev.off();
-	pdf(paste0("../images/generated/", model, "-iterations-secondary.pdf"), width = width, height = height)
-	plot(iterations$iteration, iterations$secondary, pch=18, ylab="Trajectories", xlab="Iteration");
-	lines(iterations$iteration, iterations$secondary);
-	dev.off();
-	iterations_all <- iterations$primary + iterations$secondary;
-	if (iterations_all[1] == 9) {
-		pdf(paste0("../images/generated/", model, "-iterations-summary.pdf"), width = width, height = height) 
+	for (env in unique(data.summary$environment.name)) {		
+		summary <- data.summary[data.summary$environment.name == env,];
+		if (env == "dist") {
+			xlab <- "Machines";
+		} else {
+			xlab <- "Cores";
+		}
+		# times
+		pdf(paste0("../images/generated/", model, "-", env, "-time.pdf"), width = width, height = height)
+		par(mar = mar);
+		plot(summary$environment.size, summary$time, pch=18, ylab="Time (s)", log = "x", xlab=xlab, ylim=c(0, max(summary$time)), xlim=c(1, 16), xaxt = "n");
+		lines(summary$environment.size, summary$time);
+		size_min <- min(summary$environment.size);
+		size_max <- max(summary$environment.size);
+		time_max <- max(summary$time);
 		f <- function(x) {
-			(2^x + 1)^2
+			y = (time_max / x) * size_min
+		}
+		curve(f, from = size_min, to = size_max, col="red", add=TRUE, lty=2)
+		axis(side = 1, at = c(1, 2, 4, 8, 16))
+		dev.off();
+
+		# cache
+		pdf(paste0("../images/generated/", model, "-", env, "-cache.pdf"), width = width, height = height)
+		par(mar = mar);
+		plot(summary$environment.size, summary$cache.secondary.miss, pch=18, ylab="Cache miss", xlab=xlab, log = "x", xlim=c(1, 16), xaxt = "n");
+		lines(summary$environment.size, summary$cache.secondary.miss);
+		axis(side = 1, at = c(1, 2, 4, 8, 16))
+		dev.off();
+
+		# iterations
+		iterations <- data.iterations[data.iterations$environment.name == env,]
+		iterations <- iterations[iterations$environment.size == 4,]
+		pdf(paste0("../images/generated/", model, "-", env,"-iterations-primary.pdf"), width = width, height = height)
+		par(mar = mar);
+		plot(iterations$iteration, iterations$primary, pch=18, ylab="Trajectories", xlab="Iteration");
+		lines(iterations$iteration, iterations$primary);
+		dev.off();
+		pdf(paste0("../images/generated/", model, "-", env,"-iterations-secondary.pdf"), width = width, height = height)
+		par(mar = mar);
+		plot(iterations$iteration, iterations$secondary, pch=18, ylab="Trajectories", xlab="Iteration");
+		lines(iterations$iteration, iterations$secondary);
+		dev.off();
+		iterations_all <- iterations$primary + iterations$secondary;
+		pdf(paste0("../images/generated/", model, "-", env,"-iterations-summary.pdf"), width = width, height = height);
+		par(mar = mar);
+		f <- function(x) {
+			(2^x + 1)^log(iterations_all[1], 3)
 		}
 		plot(iterations$iteration, iterations_all, pch=18, ylab="Trajectories", xlab="Iteration", ylim = c(0, f(nrow(iterations))));
 		lines(iterations$iteration, iterations_all);
 		curve(f, from = 1, to = nrow(iterations), col="red", add=TRUE, lty=2)
+		dev.off();
+	}
+
+	file.balancer <- paste0(directory, model, "-dist-16-balancer.csv")
+	if (file.exists(file.balancer)) {
+		data.balancer <- read.csv(file.balancer);
+		pdf(paste0("../images/generated/", model,"-balancer.pdf"), width = width, height = height);
+		par(mar = mar);
+		balancer <- apply(data.balancer, 1, function(x) {sum((x != 0) + 0)})
+		plot(balancer, pch=18, xlab="Balancer Ticks", ylab="Machine Usage")
+		lines(balancer)
 		dev.off();
 	}
 }
